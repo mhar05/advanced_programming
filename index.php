17ca70f233509e15d4c14802e83510d17a904bfb
mhar05 <mhar.apura@gmail.com>
2020-11-03 22:01:48 +0800

    assignment two completed

--- index.php
+++ index.php
@@ -1,75 +1,26 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-      echo "ED MHAR APURA";
-     ?>
-
-    <form class="" action="index.php" method="post">
-
-      <br> <h3>Calculator</h3>
-      <label for="num1">Input First Number:</label><br>
-      <input type="text" name="num1" value=""><br>
-      <label for="num2">Input Second Number:</label><br>
-      <input type="text" name="num2" value=""><br>
-      <input type="submit" name="add" value="+">
-      <input type="submit" name="min" value="-">
-      <input type="submit" name="mul" value="*">
-      <input type="submit" name="div" value="/">
-      <input type="submit" name="reset" value="Reset"><br>
-
-    </form>
-    <?php
-        if (isset($_POST['add'])) {
-          echo "Answer: ";
-          echo $_POST['num1'] + $_POST['num2'];
-        } else if (isset($_POST['min'])) {
-          echo "Answer: ";
-          echo $_POST['num1'] - $_POST['num2'];
-        } else if (isset($_POST['mul'])) {
-          echo "Answer: ";
-          echo $_POST['num1'] * $_POST['num2'];
-        } else if (isset($_POST['div'])) {
-          echo "Answer: ";
-          echo $_POST['num1'] / $_POST['num2'];
-        }
-    ?>
-    <form class="" action="index.php" method="post">
-      <br> <h3>Finding GCD</h3>
-      <label for="num1">Input First Number:</label><br>
-      <input type="text" name="num1" value=""><br>
-      <label for="num2">Input Second Number:</label><br>
-      <input type="text" name="num2" value=""><br>
-      <input type="submit" name="gcd" value="Submit">
-    </form>
-
-    <?php
-      if(isset($_POST['gcd']))
+<?php
+  class User
+  {
+    function __construct($name, $username, $email)
     {
-      if (!empty($_POST['num1']) && !empty($_POST['num2']))
-      {
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-        if ($num1 > $num2)
-        {
-          $temp = $num1;
-          $num1 = $num2;
-          $num2 = $temp;
-        }
-        for($i = 1; $i < ($num1+1); $i++)
-        {
-          if ($num1%$i == 0 and $num2%$i == 0)
-            $gcd = $i;
-        }
-        echo "Answer: $gcd";
-      }
+      $this->name = $name;
+      $this->username = $username;
+      $this->email = $email;
     }
-     ?>
 
+    function addFriend()
+    {
+      echo $this->name . " added a friend";
+      echo "<br>";
+    }
 
-  </body>
-</html>
+    function postStatus()
+    {
+      echo $this->name . " posted a status";
+    }
+  }
+
+  $user = new User("Ed Mhar", "mhrapura", "mhar.apura@gmail.com");
+  $user->addFriend();
+  $user->postStatus();
+?>
